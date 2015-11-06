037217bea74cb23aac6c9d487fe227eb64b5b60d
Shawn Ma <qsma@thoughtworks.com>
2015-11-06 12:15:17 +0800

    update

--- 9.1/docker-entrypoint.sh
+++ 9.1/docker-entrypoint.sh
@@ -3,19 +3,24 @@
 #enable job control in script
 set -e -m
 
-#run in background
+#####   variables  #####  
+: ${POSTGRES_USER:=postgres}
+: ${POSTGRES_DATABASE:=$POSTGRES_USER}
+: ${POSTGRES_PASSWORD:=$POSTGRES_USER}   
+: ${POSTGRES_EXTENSIONS_POSTGIS:=false}   
+
+# add command if needed
+if [ "${1:0:1}" = '-' ]; then
+  set -- postgres "$@"
+fi
+
+#run command in background
 if [ "$1" = 'postgres' ]; then
-  #####   variables  #####  
-  : ${POSTGRES_USER:=postgres}
-  : ${POSTGRES_DATABASE:=$POSTGRES_USER}
-  : ${POSTGRES_PASSWORD:=$POSTGRES_USER}   
-  : ${POSTGRES_EXTENSIONS_POSTGIS:=false}     
-  
   ##### pre scripts  #####
   echo "========================================================================"
-  echo "prepare the environment:"
+  echo "initialize:"
   echo "========================================================================"
-  chown -R postgres "$PGDATA"
+  mkdir -p $PGDATA && chown -R postgres:postgres "$PGDATA"
   if [ ! -s "$PGDATA/PG_VERSION" ]; then
     gosu postgres initdb --locale=en_US.UTF-8
     # set auth method
@@ -23,8 +28,11 @@ if [ "$1" = 'postgres' ]; then
     # set listen addresses
     /usr/bin/ansible local -o -c local -m lineinfile  -a "dest=$PGDATA/postgresql.conf regexp='^#(listen_addresses\s*=\s*)\S+' line=\"listen_addresses = '*'\""
   fi
-
+  
   ##### run scripts  #####
+  echo "========================================================================"
+  echo "startup:"
+  echo "========================================================================"
   exec gosu postgres "$@" &
   for i in {30..0}; do
     if echo 'SELECT 1' | psql --username postgres &> /dev/null; then
@@ -42,7 +50,7 @@ if [ "$1" = 'postgres' ]; then
 
   ##### post scripts #####
   echo "========================================================================"
-  echo "configure the environment:"
+  echo "configure:"
   echo "========================================================================"
   if [ "$POSTGRES_DATABASE" ]; then
     /usr/bin/ansible local -o -c local -m postgresql_db  -a "name=$POSTGRES_DATABASE encoding=UTF-8"
@@ -58,9 +66,8 @@ if [ "$1" = 'postgres' ]; then
     /usr/bin/ansible local -o -c local -m postgresql_ext -a "db=$POSTGRES_DATABASE name=postgis_topology"
   fi
   
-  #bring to foreground
+  #bring command to foreground
   fg
 else
   exec "$@"
 fi
-
--- README.md
+++ README.md
@@ -1,26 +1,20 @@
-# baselibrary/postgres [![Docker Repository on Quay.io](https://quay.io/repository/baselibrary/postgres/status "Docker Repository on Quay.io")](https://quay.io/repository/baselibrary/postgres)
+## ThoughtWorks Docker Image: postgres
 
-## Installation and Usage
+[![](http://dockeri.co/image/baselibrary/postgres)](https://registry.hub.docker.com/u/baselibrary/postgres/)
 
-    docker pull quay.io/baselibrary/postgres:${VERSION:-latest}
-
-## Available Versions (Tags)
+### Base Docker Image
 
 * `latest`: postgres 9.4
-* `9.0`: postgres 9.0
-* `9.1`: postgres 9.1
-* `9.2`: postgres 9.2
-* `9.3`: postgres 9.3
-* `9.4`: postgres 9.4
+* `9.4`   : postgres 9.4
+* `9.3`   : postgres 9.3
+* `9.2`   : postgres 9.2
+* `9.1`   : postgres 9.1
+* `9.0`   : postgres 9.0
 
-## Deployment
+### Installation
 
-To push the Docker image to Quay, run the following command:
+    docker pull baselibrary/postgres
 
-    make release
+### Usage
 
-## Continuous Integration
-
-Images are built and pushed to Docker Hub on every deploy. Because Quay currently only supports build triggers where the Docker tag name exactly matches a GitHub branch/tag name, we must run the following script to synchronize all our remote branches after a merge to master:
-
-    make sync-branches
+    docker run -it --rm baselibrary/postgres
